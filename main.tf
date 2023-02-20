f2d5bb19f337ee2ca5deb58510ce50521aed7379
Emokhare <emokharesam@gmail.com>
2023-02-20 17:30:04 +0100

    Creating an ELB for the intsance

--- main.tf
+++ main.tf
@@ -7,12 +7,17 @@ terraform {
   }
 }
 
+
+
 resource "aws_instance" "EC2" {
   count = 3
   instance_type = var.IT
   ami           = var.Num
+  security_groups = ["${aws_security_group.EC2.name}"]
   
   tags = {
    Name = "EC2"
   }
 }
+
+
